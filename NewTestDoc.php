a15655cbdbebbafe20207159518fb78df2a82266
Prashant <prashant@seffcon.in>
2018-11-16 15:22:57 +0530

    new commit1

--- NewTestDoc.php
+++ NewTestDoc.php
@@ -1,7 +1,6 @@
 <?php
 echo "string my testing11 -- Test edit  test";
-echo "second line First Commit";
-
+echo "second line First Commit
 // git ftp init --user prashant@seffcon.in --passwd seffcon@123 https://github.com/PrashantRajput159/TestProject.git
 ?>
 
