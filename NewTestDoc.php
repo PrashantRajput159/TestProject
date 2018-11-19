024b35ef9ff7f427695fb47ac5840b87b3666b3a
Prashant <prashant@seffcon.in>
2018-11-19 09:55:02 +0530

    phing test

--- NewTestDoc.php
+++
@@ -1,7 +0,0 @@
-<?php
-echo "string my testing11 -- Test edit  test";
-echo "second line First Commit
-// git ftp init --user prashant@seffcon.in --passwd seffcon@123 https://github.com/PrashantRajput159/TestProject.git
-?>
-
-
